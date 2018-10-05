aa163211f29c4e4e8bcca69adacfaecf90ef93bf
Dylan <dylan.fox@monash.edu>
2018-10-05 14:10:38 +1000

    Finished Efficiency Script
    Finished comparing actual path taken vs ideal path

--- EfficiencyPath.m
+++ EfficiencyPath.m
@@ -7,15 +7,37 @@ unfilt_F1866.midpoint{i,1} = 0;
 end 
 
 for i = 1:height(unfilt) 
-   filt.midpoint{i,:} = [(filt.thumbXYposn{i,1}(:,1)+filt.indexXYposn{i,1}(:,1))/2,(filt.thumbXYposn{i,1}(:,2)+filt.indexXYposn{i,1}(:,2))/2]
+   filt.midpoint{i,:} = [(filt.thumbXYposn{i,1}(:,1)+filt.indexXYposn{i,1}(:,1))/2,(filt.thumbXYposn{i,1}(:,2)+filt.indexXYposn{i,1}(:,2))/2];
 end 
 
 %Draw a line from the start point to the end point of the midpoint
 %data, this will be the most efficient path - calculate its distance 
-X = [filt.midpoint{1,1}(1,:); filt.midpoint{1,1}(end,:)];
+
+%Single
+X = [filt.midpoint{1,1}(1,:); filt.midpoint{1,1}(end,:)]; %Singular
 dist = pdist(X,'euclidean'); 
+
+%Iterative 
+for i = 1:height(unfilt_F1866)
+    X{i,1} = [unfilt_F1866.midpoint{i,1}(1,:); unfilt_F1866.midpoint{i,1}(end,:)];
+    ideal_dist(i,1) = pdist(X{i},'euclidean'); 
+end
 
 %Calculate distance of path actually taken using all the points recorded -
 %this will be an approximation 
-for i = 1:height(unfilt) 
-    XX = [filt.midpoint{i,1}(1,:); filt.midpoint{i,1}(end,:)] 
+
+%Single 
+X = unfilt_F1866.midpoint{1,1}; 
+a = pdist(X([1:2],:),'euclidean')
+aa = pdist(X([2:3],:),'euclidean')
+
+%WORKED - Iterative 
+for i = 1:height(unfilt_F1866)
+for k = 1:(length(unfilt_F1866.midpoint{i,1}) -1)
+a(k,i) = pdist(unfilt_F1866.midpoint{i,1}([k:k+1],:),'euclidean');
+end
+end
+actual_dist = sum(a,1); 
+actual_dist = actual_dist' 
+
+ReachPathRatio = actual_dist./ideal_dist
