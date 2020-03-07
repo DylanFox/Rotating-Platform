3f68045d82a8ad99bc774e17304d0b4b9734b28e
Dylan <dylan.fox@monash.edu>
2020-03-07 14:53:14 +1100

    Added alldata and changed bootstrap example

--- BootstrapExample.m
+++ BootstrapExample.m
@@ -1,20 +1,10 @@
 %Bootstrapping example 
-[M1778_TrialTypeSpeed,TID1,TID2] = findgroups(filt_M1778.trialtype,filt_M1778.Speed) %Split into unique groups of trial type and speed
-bootstat1 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==1));  %Start bootstrap for each group 
-bootstat2 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==2));
-bootstat3 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==3));
-bootstat4 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==4));
-bootstat5 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==5));
-bootstat6 = bootstrp(10000,@mean,filt_M1778.mMGA(M1778_TrialTypeSpeed==6));
-figure(4) %Draw figures comparing groups you need. 
-histogram(bootstat1,30)
+AllAnimals_filt.mMGA(AllAnimals_filt.cohort == 'Control')
+bootstat_control_mga = bootstrp(10000,@mean,AllAnimals_filt.mMGA(AllAnimals_filt.cohort == 'Control'));
+bootstat_pimv1_mga = bootstrp(10000,@mean,AllAnimals_filt.mMGA(AllAnimals_filt.cohort == 'PIm + V1'));
+bootstat_lgnv1_mga = bootstrp(10000,@mean,AllAnimals_filt.mMGA(AllAnimals_filt.cohort == 'LGN + V1'));
+figure('Name','MGA Distributed Means')
+histogram(bootstat_control_mga,30)
 hold on
-histogram(bootstat4,30)
-figure (5)
-histogram(bootstat2,30)
-hold on
-histogram(bootstat5,30)
-figure (6)
-histogram(bootstat3,30)
-hold on
-histogram(bootstat6,30)
+histogram(bootstat_pimv1_mga,30)
+histogram(bootstat_lgnv1_mga,30)
